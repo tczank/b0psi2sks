7f8297658a08ea326b49e6f38e107adcc3df1e55
thomas rafael czank <thomas.czank@gmail.com>
2019-07-24 14:22:46 +0900

    flavor tagger works but only when basf2 is run from kekcc

--- psi2smumu_1.py
+++ psi2smumu_1.py
@@ -9,12 +9,16 @@
 ## new preamble for release 04 ###
 import basf2 as b2
 import modularAnalysis as ma
+import flavorTagger as ft
 import variables.collections as vc
 import variables.utils as vu
+import variables as v
 mypath = b2.Path()
 
 print(mypath)
 # input mdst file
+# the flavor tagger weighting file only works from kekcc
+#b2.use_central_database("analysis_tools_release-03-01-00")
 ma.inputMdst(environmentType='default',
              filename=b2.find_file('/home/thczank/b0psi2sks/gsim/psi2smumu/b0_psi2s_gsim_psi2smumu_all.root'),
              path=mypath)
@@ -25,9 +29,9 @@ ma.inputMdst(environmentType='default',
 
 ################################################################################
 ma.fillParticleList('K+:all',  '',path=mypath)
-ma.fillParticleList('mu+:all', '',path=mypath)
+ma.fillParticleList('mu+:all', cut='muonID > 0.01',path=mypath)
 ma.fillParticleList('pi+:all', '',path=mypath)
-ma.fillParticleList('e+:all', '',path=mypath)
+ma.fillParticleList('e+:all', cut='electronID > 0.01 and d0 < 2 and abs(z0) < 4',path=mypath)
 
 ################################################################################
 
@@ -35,29 +39,36 @@ ma.fillParticleList('e+:all', '',path=mypath)
 ma.cutAndCopyList("mu+:gen", "mu+:all", "charge>0",path=mypath)
 ma.cutAndCopyList("mu-:gen", "mu+:all", "charge<0",path=mypath)
 ma.reconstructDecay(decayString="psi(2S):gen -> mu+:gen mu-:gen",
-                    cut="",
+                    cut="3.5 < M < 3.8 ",
                     path=mypath)
 ma.matchMCTruth(list_name="psi(2S):gen",path=mypath)
+
+v.variables.addAlias('mu_EoP','daughter(0,clusterEoP)')
+v.variables.addAlias('psi2smuid','daughter(0,muonID)')
 
 chiProb=["chiProb"]
 EoP=['clusterEoP']
 piid=['pionID']
+costheta=['cosTheta']
+p123=['px','py','pz']
+issig=['isSignal']
 
-
-
-psi2s_vars = vc.mc_truth + vc.kinematics + vc.inv_mass + vc.mc_variables + chiProb + EoP + piid
+psi2s_vars = vc.mc_truth + vc.kinematics + vc.inv_mass + vc.mc_variables + chiProb + EoP + piid + p123 + issig
 
 ma.cutAndCopyList("pi+:rec", "pi+:all", "charge>0",path=mypath)
 ma.cutAndCopyList("pi-:rec", "pi+:all", "charge<0",path=mypath)
-ma.reconstructDecay("K_S0:rec -> pi+:rec pi-:rec","",path=mypath)
+ma.reconstructDecay("K_S0:rec -> pi+:rec pi-:rec",cut="0.3 < M < 0.7 ",path=mypath)
 ma.matchMCTruth("K_S0:rec",path=mypath)
 
-ma.cutAndCopyList("K_S0:rectru", "K_S0:rec", "mcErrors == 0",path=mypath)
-ma.cutAndCopyList("psi(2S):rectru", "psi(2S):gen", "mcErrors == 0",path=mypath)
+ma.cutAndCopyList("K_S0:rectru", "K_S0:rec", cut="",path=mypath)
+ma.cutAndCopyList("psi(2S):rectru", "psi(2S):gen", cut="",path=mypath)
 
-ma.reconstructDecay("B0:recgen -> psi(2S):rectru K_S0:rectru","",path=mypath)
+ma.reconstructDecay("B0:recgen -> psi(2S):rectru K_S0:rectru",cut="Mbc > 5.2 and abs(deltaE)<0.15 ",path=mypath)
 
-b0_vars = vc.mc_truth + vc.kinematics + vc.deltae_mbc + vc.mc_variables + vc.mc_vertex + vc.mc_tag_vertex + vc.tag_vertex + vc.track + vc.vertex + chiProb + EoP + piid
+
+#b0mcflavor = ['mcFlavorOfOtherB0']
+
+b0_vars = vc.mc_truth + vc.kinematics + vc.deltae_mbc + vc.mc_variables + vc.mc_vertex + vc.mc_tag_vertex + vc.tag_vertex + vc.track + vc.vertex + chiProb + EoP + piid + issig #+ b0mcflavor
 
 ######################################################################################
 
@@ -70,14 +81,14 @@ ma.vertexRave("B0:recgen",-1, "B0:recgen -> [psi(2S):rectru -> ^mu+ ^mu- ]", "ip
 
 
 ma.matchMCTruth("B0:recgen",path=mypath)
-ma.buildRestOfEvent("B0:recgen",path=mypath)
+ma.buildRestOfEvent(target_list_name='B0:recgen',path=mypath)
+ft.flavorTagger( particleLists=['B0:recgen'], weightFiles='B2JpsiKs_muBGx1',path=my_path)
 ma.TagV("B0:recgen", "breco", 0.001, "standard_PXD",path=mypath)
-
 
 #############################################################################
 
 ########################### Saving variables to ntuple ##############################
-rootOutputFile = "B0psi2Smumu_recon_realdat_test.root"
+rootOutputFile = "B0psi2Smumu_yusa_test.root"
 
 ma.variablesToNtuple(decayString="psi(2S):gen",
                   variables=psi2s_vars,
